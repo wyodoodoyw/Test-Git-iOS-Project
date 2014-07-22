5b86ac0acb78664346ddd2d58e7b69bb5ed7e40c
wyodoodoyw <mwood051@gmail.com>
2014-07-22 13:12:04 -0400

    added test class

---
+++ Git Test/MLWTest.h
@@ -0,0 +1,13 @@
+//
+//  MLWTest.h
+//  Git Test
+//
+//  Created by Matthew Wood on 7/22/14.
+//  Copyright (c) 2014 Matthew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MLWTest : NSObject
+
+@end
---
+++ Git Test/MLWTest.m
@@ -0,0 +1,13 @@
+//
+//  MLWTest.m
+//  Git Test
+//
+//  Created by Matthew Wood on 7/22/14.
+//  Copyright (c) 2014 Matthew. All rights reserved.
+//
+
+#import "MLWTest.h"
+
+@implementation MLWTest
+
+@end
